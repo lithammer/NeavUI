8020600f3e491192c800b4a2743137b8cc7f27f7
Peter Renström <renstrom.peter@gmail.com>
2010-11-11 12:27:56 +0100

    Removed some Tukui specific code from oUF_AuraWatch

--- Interface/AddOns/oUF_AuraWatch/oUF_AuraWatch.lua
+++ Interface/AddOns/oUF_AuraWatch/oUF_AuraWatch.lua
@@ -1,6 +1,3 @@
-if TukuiCF.unitframes.enable ~= true or TukuiCF.unitframes.raidunitdebuffwatch ~= true then return end
-if not oUF then return end
-
 --[[------------------------------------------------------------------------------------------------------
 oUF_AuraWatch by Astromech
 Please leave comments, suggestions, and bug reports on this addon's WoWInterface page
