d2ab2f4449d39b52e4f45ed924f4e0ef5c537b67
unknown <renstrom.peter@gmail.com>
2010-12-21 01:12:09 +0100

    Updated a few libs, hopefully the didn't break anything

--- Interface/AddOns/oUF_Neav/modules/oUF_Smooth.lua
+++ Interface/AddOns/oUF_Neav/modules/oUF_Smooth.lua
@@ -1,12 +1,17 @@
-if not oUF then return end
+local _, ns = ...
+local oUF = ns.oUF or oUF
+assert(oUF, "<name> was unable to locate oUF install.")
 
 local smoothing = {}
 local function Smooth(self, value)
-	if value ~= self:GetValue() or value == 0 then
-		smoothing[self] = value
-	else
+	local _, max = self:GetMinMaxValues()
+	if value == self:GetValue() or (self._max and self._max ~= max) then
 		smoothing[self] = nil
+		self:SetValue_(value)
+	else
+		smoothing[self] = value
 	end
+	self._max = max
 end
 
 local function SmoothBar(self, bar)
@@ -24,16 +29,19 @@ local function hook(frame)
 	end
 end
 
+
 for i, frame in ipairs(oUF.objects) do hook(frame) end
 oUF:RegisterInitCallback(hook)
 
-local f, min, max = CreateFrame('Frame'), math.min, math.max
+
+local f, min, max = CreateFrame('Frame'), math.min, math.max 
 f:SetScript('OnUpdate', function()
 	local limit = 30/GetFramerate()
 	for bar, value in pairs(smoothing) do
 		local cur = bar:GetValue()
 		local new = cur + min((value-cur)/3, max(value-cur, limit))
 		if new ~= new then
+			-- Mad hax to prevent QNAN.
 			new = value
 		end
 		bar:SetValue_(new)
--- Interface/AddOns/oUF_Neav/modules/oUF_SpellRange.lua
+++ Interface/AddOns/oUF_Neav/modules/oUF_SpellRange.lua
@@ -13,7 +13,7 @@
   ****************************************************************************]]
 
 
--- local oUF = select( 2, ... ).oUF or _G[ assert( GetAddOnMetadata( ..., "X-oUF" ), "X-oUF metadata missing in parent addon." ) ];
+--local oUF = select( 2, ... ).oUF or _G[ assert( GetAddOnMetadata( ..., "X-oUF" ), "X-oUF metadata missing in parent addon." ) ];
 assert( oUF, "Unable to locate oUF." );
 
 local UpdateRate = 0.1;
@@ -119,6 +119,18 @@ do
 end
 
 
+--- Called by oUF when the unit frame's unit changes or otherwise needs a complete update.
+-- @param Event  Reason for the update.  Can be a real event, nil, or a string defined by oUF.
+local function Update ( self, Event, UnitID )
+	if ( Event ~= "OnTargetUpdate" ) then -- OnTargetUpdate is fired on a timer for *target units that don't have real events
+		ObjectRanges[ self ] = nil; -- Force update to fire
+		UpdateRange( self ); -- Update range immediately
+	end
+end
+--- Forces range to be recalculated for this element's frame immediately.
+local function ForceUpdate ( self )
+	return Update( self.__owner, "ForceUpdate", self.__owner.unit );
+end
 --- Called by oUF for new unit frames to setup range checking.
 -- @return True if the range element was actually enabled.
 local function Enable ( self, UnitID )
@@ -133,6 +145,8 @@ local function Enable ( self, UnitID )
 			self.Range = nil; -- Prevent range element from enabling, since enable order isn't stable
 		end
 
+		SpellRange.__owner = self;
+		SpellRange.ForceUpdate = ForceUpdate;
 		if ( not UpdateFrame ) then
 			UpdateFrame = CreateFrame( "Frame" );
 			UpdateFrame:SetScript( "OnUpdate", OnUpdate );
@@ -156,14 +170,6 @@ local function Disable ( self )
 		UpdateFrame:UnregisterEvent( "SPELLS_CHANGED" );
 	end
 end
---- Called by oUF when the unit frame's unit changes or otherwise needs a complete update.
--- @param Event  Reason for the update.  Can be a real event, nil, or a string defined by oUF.
-local function Update ( self, Event, UnitID )
-	if ( Event ~= "OnTargetUpdate" ) then -- OnTargetUpdate is fired on a timer for *target units that don't have real events
-		ObjectRanges[ self ] = nil; -- Force update to fire
-		UpdateRange( self ); -- Update range immediately
-	end
-end
 
 
 
@@ -173,42 +179,32 @@ local _, Class = UnitClass( "player" );
 -- First known spell in the appropriate class list gets used.
 -- Note: Spells probably shouldn't have minimum ranges!
 HelpIDs = ( {
-	-- DEATHKNIGHT = {};
-	DRUID = { 5185 }; -- Healing Touch (40yd) - Starter
+	DEATHKNIGHT = { 47541 }; -- Death Coil (40yd) - Starter
+	DRUID = { 5185 }; -- Healing Touch (40yd) - Lvl 3
 	-- HUNTER = {};
-	MAGE = {
-		475, -- Remove Curse (40yd) - Lvl 18
-		1459, -- Arcane Intellect (30yd) - Lvl 1
-	};
-	PALADIN = { 635 }; -- Holy Light (40yd) - Starter
-	PRIEST = { 2050 }; -- Lesser Heal (40yd) - Starter
-	SHAMAN = { 331 }; -- Healing Wave (40yd) - Starter
+	MAGE = { 475 }; -- Remove Curse (40yd) - Lvl 30
+	PALADIN = { 85673 }; -- Word of Glory (40yd) - Lvl 9
+	PRIEST = { 2061 }; -- Flash Heal (40yd) - Lvl 3
+	-- ROGUE = {};
+	SHAMAN = { 331 }; -- Healing Wave (40yd) - Lvl 7
 	WARLOCK = { 5697 }; -- Unending Breath (30yd) - Lvl 16
 	-- WARRIOR = {};
 } )[ Class ];
 
 HarmIDs = ( {
-	DEATHKNIGHT = { 52375 }; -- Death Coil (30yd) - Lvl 55
-	DRUID = { 5176 }; -- Wrath (30yd, 36yd talent) - Starter
-	HUNTER = { 75 }; -- Auto Shot (5-35yd) - Starter
-	MAGE = {
-		44614, -- Frostfire Bolt (40yd) - Lvl 75
-		133, -- Fireball (35yd) - Starter
-	};
+	DEATHKNIGHT = { 47541 }; -- Death Coil (30yd) - Starter
+	DRUID = { 5176 }; -- Wrath (40yd) - Starter
+	HUNTER = { 75 }; -- Auto Shot (5-40yd) - Starter
+	MAGE = { 133 }; -- Fireball (40yd) - Starter
 	PALADIN = {
-		62124, -- Hand of Reckoning (30yd) - Lvl 16
-		879, -- Exorcism (30yd) - Lvl 20
+		62124, -- Hand of Reckoning (30yd) - Lvl 14
+		879, -- Exorcism (30yd) - Lvl 18
 	};
-	PRIEST = {
-		589, -- Shadow Word: Pain (30yd, 36yd talent) - Lvl 4
-		585, -- Smite (30yd) - Starter
-	};
-	SHAMAN = { 403 }; -- Lightning Bolt (30yd, 36yd talent) - Starter
-	WARLOCK = {
-		172, -- Corruption (30yd, 36yd talent) -- Lvl 4
-		686, -- Shadow Bolt (30yd) - Starter
-	};
-	WARRIOR = { 355 }; -- Taunt (30yd) - Lvl 10
+	PRIEST = { 589 }; -- Shadow Word: Pain (40yd) - Lvl 4
+	-- ROGUE = {};
+	SHAMAN = { 403 }; -- Lightning Bolt (30yd) - Starter
+	WARLOCK = { 686 }; -- Shadow Bolt (40yd) - Starter
+	WARRIOR = { 355 }; -- Taunt (30yd) - Lvl 12
 } )[ Class ];
 
 oUF:AddElement( "SpellRange", Update, Enable, Disable );
