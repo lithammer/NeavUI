3d9ed10e70be62d7d02177e6fc90e1baf57f3e0b
ballagarba <renstrom.peter@gmail.com>
2011-02-15 13:05:50 +0100

    Re-indented the code and removed tukui specific variable

--- Interface/AddOns/nChat/nChat_TellTarget.lua
+++ Interface/AddOns/nChat/nChat_TellTarget.lua
@@ -1,18 +1,22 @@
 for i = 1, NUM_CHAT_WINDOWS do
-	local editBox = _G["ChatFrame"..i.."EditBox"]
-	editBox:HookScript("OnTextChanged", function(self)
-	   local text = self:GetText()
-	   if text:len() < 5 then
-		  if text:sub(1, 4) == "/tt " then
-			 local unitname, realm
-			 unitname, realm = UnitName("target")
-			 if unitname then unitname = gsub(unitname, " ", "") end
-			 if unitname and not UnitIsSameServer("player", "target") then
-				unitname = unitname .. "-" .. gsub(realm, " ", "")
-			 end
-			 ChatFrame_SendTell((unitname or tukuilocal.chat_invalidtarget), ChatFrame1)
-		  end
-	   end
+	local editBox = _G['ChatFrame'..i..'EditBox']
+	
+	editBox:HookScript('OnTextChanged', function(self)
+		local text = self:GetText()
+		
+		if text:len() < 5 then
+			if text:sub(1, 4) == '/tt ' then
+				local unitname, realm
+				unitname, realm = UnitName('target')
+				
+				if unitname then unitname = gsub(unitname, ' ', '') end
+					if unitname and not UnitIsSameServer('player', 'target') then
+						unitname = unitname .. '-' .. gsub(realm, ' ', '')
+					end
+
+					ChatFrame_SendTell((unitname or 'Invalid target'), ChatFrame1)
+				end
+			end
 	end)
 end
 
