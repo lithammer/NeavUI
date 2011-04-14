e7b6c344afde004f0b1cc83ee8f33196dc11b0de
Neav <anton.ickert@web.de>
2011-04-14 20:50:47 +0200

    Add config file

---
+++ Interface/AddOns/nChat/nChat_Config.lua
@@ -0,0 +1,22 @@
+
+    CHAT_FRAME_TAB_SELECTED_MOUSEOVER_ALPHA = 1.0
+    CHAT_FRAME_TAB_SELECTED_NOMOUSE_ALPHA = 0     -- set to 0 if u want to hide the tabs when no mouse is over them or the chat
+    
+    CHAT_FRAME_TAB_NORMAL_MOUSEOVER_ALPHA = 0.5
+    CHAT_FRAME_TAB_NORMAL_NOMOUSE_ALPHA = 0      -- set to 0 if u want to hide the tabs when no mouse is over them or the chat
+    
+    CHAT_FRAME_FADE_OUT_TIME = 0.5
+    CHAT_FRAME_FADE_TIME = 0.1
+
+nChat = {
+    enableURLCopy = true, 
+    chatOutline = false,
+    
+    tab = {
+        fontSize = 15,
+        fontOutline = true, 
+        normalColor = {1, 1, 1},
+        specialColor = {1, 0, 1},
+        selectedColor = {0, 0.75, 1},
+    },
+}
