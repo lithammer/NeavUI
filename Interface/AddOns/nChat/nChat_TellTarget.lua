9c0ba9f21491871367c79cd70022d12c7a2c06fe
Neav <anton.ickert@web.de>
2011-04-14 20:51:24 +0200

    make sure that telltarget only work on friendly player

--- Interface/AddOns/nChat/nChat_TellTarget.lua
+++ Interface/AddOns/nChat/nChat_TellTarget.lua
@@ -4,27 +4,23 @@ for i = 1, NUM_CHAT_WINDOWS do
 	editBox:HookScript('OnTextChanged', function(self)
 		local text = self:GetText()
 		
-		if text:len() < 5 then
-			if text:sub(1, 4) == '/tt ' then
-				local unitname, realm
-				unitname, realm = UnitName('target')
-				
-				if unitname then unitname = gsub(unitname, ' ', '') end
-					if unitname and not UnitIsSameServer('player', 'target') then
-						unitname = unitname .. '-' .. gsub(realm, ' ', '')
-					end
+        if (UnitExists('target') and UnitIsPlayer('target') and UnitIsFriend('player', 'target')) then
+            if (text:len() < 5) then
+                if (text:sub(1, 4) == '/tt ') then
+                    local unitname, realm
+                    unitname, realm = UnitName('target')
+                    
+                    if (unitname) then 
+                        unitname = gsub(unitname, ' ', '') 
+                    end
+                    
+                    if (unitname and not UnitIsSameServer('player', 'target')) then
+                        unitname = unitname .. '-' .. gsub(realm, ' ', '')
+                    end
 
-					ChatFrame_SendTell((unitname or 'Invalid target'), ChatFrame1)
-				end
-			end
+                    ChatFrame_SendTell((unitname or 'Invalid target'), ChatFrame1)
+                end
+            end
+        end
 	end)
 end
-
-
--- SlashCmdList['TELLTARGET'] = function(msg)   
-	-- if (UnitExists('target') and UnitIsPlayer('target') and UnitIsFriend('player', 'target')) then
-		-- SendChatMessage(msg, 'WHISPER', nil, UnitName('target'))
-	-- end
--- end
-
--- SLASH_TELLTARGET1 = '/tt'
