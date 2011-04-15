bb82369b8d50d4e27668cda698b922ad3136f0af
Neav <anton.ickert@web.de>
2011-04-15 14:51:52 +0200

    a config file, yeay

---
+++ Interface/AddOns/nTooltip/nTooltip_Config.lua
@@ -0,0 +1,17 @@
+
+nTooltip = {
+    fontSize = 14,
+        
+    showMouseoverTarget = true,
+    showTooltipHealth = true,
+    
+    abbrevRealmNames = false, 
+    
+    healthbar = {
+        fontSize = 13,
+        font = 'Fonts\\ARIALN.ttf',
+        showOutline = false,
+        textPos = 'CENTER',    -- possible 'TOP' 'BOTTOM' 'CENTER'
+    },
+}
+
