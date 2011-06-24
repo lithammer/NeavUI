7124fdc37b85bed25196a9616de78a7cc2808dce
Neav <anton.ickert@web.de>
2011-06-24 17:28:19 +0200

    improve row calc

--- Interface/AddOns/nBuff/config.lua
+++ Interface/AddOns/nBuff/config.lua
@@ -1,25 +1,23 @@
 
 nBuff = {
-    button = {
-        size = 36,
-        scale = 1,
-        
-        buffPerRow = 9,
-        
-        buffBorderColor     = {1, 1, 1},
-        
-        durationFontsize    = 15,
-        durationFont        = 'Fonts\\ARIALN.ttf',
-        
-        countFontsize       = 17,
-        countFont           = 'Fonts\\ARIALN.ttf',
-    
-        borderBuff          = 'Interface\\AddOns\\nBuff\\media\\textureOverlay',
-        borderDebuff        = 'Interface\\AddOns\\nBuff\\media\\textureDebuff',
-    },
-    
-    padding = {
-        x = 7,
-        y = 7,
-    },
+    buffSize = 36,
+    buffScale = 1,
+    buffBorderColor = {1, 1, 1},   
+
+    debuffSize = 36,
+    debuffScale = 1,
+
+    buffPerRow = 9,
+
+    durationFontsize = 15,
+    durationFont = 'Fonts\\ARIALN.ttf',
+
+    countFontsize = 17,
+    countFont = 'Fonts\\ARIALN.ttf',
+
+    borderBuff = 'Interface\\AddOns\\nBuff\\media\\textureOverlay',
+    borderDebuff = 'Interface\\AddOns\\nBuff\\media\\textureDebuff',
+
+    paddingX = 7,
+    paddingY = 7,
 }
--- Interface/AddOns/nBuff/core.lua
+++ Interface/AddOns/nBuff/core.lua
@@ -11,12 +11,14 @@ HOUR_ONELETTER_ABBR   = '|cffffffff%dh|r'
 MINUTE_ONELETTER_ABBR = '|cffffffff%dm|r'
 SECOND_ONELETTER_ABBR = '|cffffffff%d|r'
 
+-- DEBUFF_MAX_DISPLAY = 32
+
 TemporaryEnchantFrame:ClearAllPoints()
 TemporaryEnchantFrame:SetPoint('TOPRIGHT', Minimap, 'TOPLEFT', -15, 0)
 TemporaryEnchantFrame.SetPoint = function() end
 
 TempEnchant2:ClearAllPoints()
-TempEnchant2:SetPoint('TOPRIGHT', TempEnchant1, 'TOPLEFT', -nBuff.padding.x, 0)
+TempEnchant2:SetPoint('TOPRIGHT', TempEnchant1, 'TOPLEFT', -nBuff.paddingX, 0)
 
 ConsolidatedBuffs:SetSize(20, 20)
 ConsolidatedBuffs:ClearAllPoints()
@@ -43,13 +45,13 @@ local function BuffFrame_SetPoint(self)
             return
         else
             if (hasMainHandEnchant and hasOffHandEnchant and hasThrownEnchant) then
-                self:SetPoint('TOPRIGHT', TempEnchant3, 'TOPLEFT', -nBuff.padding.x, 0)
+                self:SetPoint('TOPRIGHT', TempEnchant3, 'TOPLEFT', -nBuff.paddingX, 0)
                 return
 			elseif (hasMainHandEnchant and hasOffHandEnchant) or (hasMainHandEnchant and hasThrownEnchant) or (hasOffHandEnchant and hasThrownEnchant) then
-				self:SetPoint('TOPRIGHT', TempEnchant2, 'TOPLEFT', -nBuff.padding.x, 0)
+				self:SetPoint('TOPRIGHT', TempEnchant2, 'TOPLEFT', -nBuff.paddingX, 0)
 				return
             elseif (hasMainHandEnchant or hasOffHandEnchant or hasThrownEnchant) then
-                self:SetPoint('TOPRIGHT', TempEnchant1, 'TOPLEFT', -nBuff.padding.x, 0)
+                self:SetPoint('TOPRIGHT', TempEnchant1, 'TOPLEFT', -nBuff.paddingX, 0)
                 return
             elseif (not hasMainHandEnchant and not hasOffHandEnchant and not hasThrownEnchant) then
                 self:SetPoint('TOPRIGHT', TempEnchant1)
@@ -79,11 +81,12 @@ end)
 
 hooksecurefunc('BuffFrame_UpdateAllBuffAnchors', function()  
     local BUFF_PREVIOUS, BUFF_ABOVE
-	local numBuffs = 0
-
+	-- local numBuffs = 0
+    local numBuffs = BuffFrame.numEnchants 
+    
 	for i = 1, BUFF_ACTUAL_DISPLAY do
 		local buff = _G['BuffButton'..i]
-
+        
 		if (buff.consolidated) then
 			if (buff.parent == BuffFrame) then
 				buff:SetParent(ConsolidatedBuffsContainer)
@@ -91,24 +94,26 @@ hooksecurefunc('BuffFrame_UpdateAllBuffAnchors', function()
 			end
 		else
 			numBuffs = numBuffs + 1
-        
+            
 			if (buff.parent ~= BuffFrame) then
 				buff:SetParent(BuffFrame)
                 buff.parent = BuffFrame
 			end
                 
             buff:ClearAllPoints()
-            if (numBuffs > 1 and mod(numBuffs, nBuff.button.buffPerRow) == 1) then
-                if (numBuffs == nBuff.button.buffPerRow + 1) then
-                    buff:SetPoint('TOP', TempEnchant1, 'BOTTOM', 0, -nBuff.padding.y)
+            
+            if (numBuffs > 1 and mod(numBuffs, nBuff.buffPerRow) == 1) then
+                if (numBuffs == nBuff.buffPerRow + 1) then
+                    buff:SetPoint('TOP', TempEnchant1, 'BOTTOM', 0, -nBuff.paddingY)
                 else
-                    buff:SetPoint('TOP', BUFF_ABOVE, 'BOTTOM', 0, -nBuff.padding.y)
+                    buff:SetPoint('TOP', BUFF_ABOVE, 'BOTTOM', 0, -nBuff.paddingY)
                 end
+                
                 BUFF_ABOVE = buff
             elseif (numBuffs == 1) then
                 BuffFrame_SetPoint(buff)
             else
-                buff:SetPoint('RIGHT', BUFF_PREVIOUS, 'LEFT', -nBuff.padding.x, 0)
+                buff:SetPoint('RIGHT', BUFF_PREVIOUS, 'LEFT', -nBuff.paddingX, 0)
             end
             
             BUFF_PREVIOUS = buff
@@ -117,34 +122,38 @@ hooksecurefunc('BuffFrame_UpdateAllBuffAnchors', function()
 end)
 
 hooksecurefunc('DebuffButton_UpdateAnchors', function(self, index)
-    local BUFF_NEW_SPACE, BUFF_NEW_ROW, BUFF_NUM_ROWS, BUFF_NUM_BUFFS
-
-    BUFF_NEW_SPACE = 31 + nBuff.padding.y
-    BUFF_NUM_BUFFS = (BUFF_ACTUAL_DISPLAY > 0 and BUFF_ACTUAL_DISPLAY) or 1
-    BUFF_NUM_ROWS  = ceil(BUFF_NUM_BUFFS/nBuff.button.buffPerRow)
+    local numBuffs = BUFF_ACTUAL_DISPLAY + BuffFrame.numEnchants
     
-    if (BUFF_NUM_ROWS and BUFF_NUM_ROWS > 1) then
-        BUFF_NEW_ROW = -BUFF_NUM_ROWS * BUFF_NEW_SPACE
+	if (BuffFrame.numConsolidated > 0) then
+		numBuffs = numBuffs - BuffFrame.numConsolidated + 1
+	end
+
+    local debuffSpace = nBuff.buffSize + nBuff.paddingY
+    local numRows = ceil(numBuffs/nBuff.buffPerRow)
+
+    local rowSpacing
+    if (numRows and numRows > 1) then
+        rowSpacing = -numRows * debuffSpace
     else
-        BUFF_NEW_ROW = -BUFF_NEW_SPACE
+        rowSpacing = -debuffSpace
     end
 
     local buff = _G[self..index]
     buff:ClearAllPoints()
+    
     if (index == 1) then
-        buff:SetPoint('TOP', TempEnchant1, 'BOTTOM', 0, BUFF_NEW_ROW)
-	elseif (index >= 2 and mod(index, nBuff.button.buffPerRow) == 1) then
-		buff:SetPoint('TOP', _G[self..(index-nBuff.button.buffPerRow)], 'BOTTOM', 0, -nBuff.padding.y)
+        buff:SetPoint('TOP', TempEnchant1, 'BOTTOM', 0, rowSpacing)
+	elseif (index >= 2 and mod(index, nBuff.buffPerRow) == 1) then
+		buff:SetPoint('TOP', _G[self..(index-nBuff.buffPerRow)], 'BOTTOM', 0, -nBuff.paddingY)
 	else
-		buff:SetPoint('RIGHT', _G[self..(index-1)], 'LEFT', -nBuff.padding.x, 0)
+		buff:SetPoint('RIGHT', _G[self..(index-1)], 'LEFT', -nBuff.paddingX, 0)
 	end
 end)
 
 for i = 1, 3 do
     local button = _G['TempEnchant'..i]
-    button:SetScale(nBuff.button.scale)
-    button:SetWidth(nBuff.button.size)
-    button:SetHeight(nBuff.button.size)
+    button:SetScale(nBuff.buffScale)
+    button:SetSize(nBuff.buffSize, nBuff.buffSize)
 
     local icon = _G['TempEnchant'..i..'Icon']
     icon:SetTexCoord(0.03, 0.97, 0.03, 0.97)
@@ -152,7 +161,7 @@ for i = 1, 3 do
     local duration = _G['TempEnchant'..i..'Duration']
     duration:ClearAllPoints()
     duration:SetPoint('BOTTOM', button, 'BOTTOM', 0, -2)
-    duration:SetFont(nBuff.button.durationFont, nBuff.button.durationFontsize,'OUTLINE')
+    duration:SetFont(nBuff.durationFont, nBuff.durationFontsize,'OUTLINE')
     duration:SetShadowOffset(0, 0)
     duration:SetDrawLayer('OVERLAY')
 
@@ -160,7 +169,7 @@ for i = 1, 3 do
     border:ClearAllPoints()
     border:SetPoint('TOPRIGHT', button, 1, 1)
     border:SetPoint('BOTTOMLEFT', button, -1, -1)    
-    border:SetTexture(nBuff.button.borderDebuff)
+    border:SetTexture(nBuff.borderDebuff)
     border:SetTexCoord(0, 1, 0, 1)
     border:SetVertexColor(0.9, 0.25, 0.9)
 
@@ -174,9 +183,13 @@ end
 hooksecurefunc('AuraButton_Update', function(self, index)
     local button = _G[self..index]
     if (button) then
-        button:SetWidth(nBuff.button.size)
-        button:SetHeight(nBuff.button.size)
-        button:SetScale(nBuff.button.scale)
+        if (self:match('Debuff')) then
+            button:SetSize(nBuff.debuffSize, nBuff.debuffSize)
+            button:SetScale(nBuff.debuffScale)
+        else
+            button:SetSize(nBuff.buffSize, nBuff.buffSize)
+            button:SetScale(nBuff.buffScale)
+        end
     end
         
     local icon = _G[self..index..'Icon']
@@ -188,7 +201,7 @@ hooksecurefunc('AuraButton_Update', function(self, index)
     if (duration) then
         duration:ClearAllPoints()
         duration:SetPoint('BOTTOM', button, 'BOTTOM', 0, -2)
-        duration:SetFont(nBuff.button.durationFont, nBuff.button.durationFontsize,'OUTLINE')
+        duration:SetFont(nBuff.durationFont, nBuff.durationFontsize, 'OUTLINE')
         duration:SetShadowOffset(0, 0)
         duration:SetDrawLayer('OVERLAY')
     end
@@ -197,14 +210,14 @@ hooksecurefunc('AuraButton_Update', function(self, index)
     if (count) then
         count:ClearAllPoints()
         count:SetPoint('TOPRIGHT', button)
-        count:SetFont(nBuff.button.countFont, nBuff.button.countFontsize, 'OUTLINE')
+        count:SetFont(nBuff.countFont, nBuff.countFontsize, 'OUTLINE')
         count:SetShadowOffset(0, 0)
         count:SetDrawLayer('OVERLAY')
     end
         
     local border = _G[self..index..'Border']
     if (border) then
-        border:SetTexture(nBuff.button.borderDebuff)
+        border:SetTexture(nBuff.borderDebuff)
         border:SetPoint('TOPRIGHT', button, 1, 1)
         border:SetPoint('BOTTOMLEFT', button, -1, -1)
         border:SetTexCoord(0, 1, 0, 1)
@@ -214,10 +227,10 @@ hooksecurefunc('AuraButton_Update', function(self, index)
         if (not button.texture) then
             button.texture = button:CreateTexture('$parentOverlay', 'ARTWORK')
             button.texture:SetParent(button)
-            button.texture:SetTexture(nBuff.button.borderBuff)
+            button.texture:SetTexture(nBuff.borderBuff)
             button.texture:SetPoint('TOPRIGHT', button, 1, 1)
             button.texture:SetPoint('BOTTOMLEFT', button, -1, -1)
-            button.texture:SetVertexColor(unpack(nBuff.button.buffBorderColor))
+            button.texture:SetVertexColor(unpack(nBuff.buffBorderColor))
         end
     end
     
