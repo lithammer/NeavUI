3d542c7811f2403aaff161820a85c31fe7275cf2
Neav <anton.ickert@web.de>
2011-07-05 16:09:26 +0200

    upped the wrong file

--- Interface/AddOns/nMinimap/config.lua
+++ Interface/AddOns/nMinimap/config.lua
@@ -2,8 +2,9 @@
 nMinimap = {
     drawerMouseoverAlpha = 1,
     drawerNoMouseoverAlpha = 0.55,
-	positionDrawerBelow = false, -- position the guild/friends drawer below the minimap instead of above
-	showDrawerOnMinimapMouseOver = false, -- shows the guild/friends drawer on mouse-over on the minimap
+    positionDrawerBelow = false,            -- Position the guild/friends drawer below the minimap instead of above
+    showDrawerOnMinimapMouseOver = false,   -- Shows the guild/friends drawer on mouse-over on the minimap
+    alwaysShowDrawer = false,               -- Always shows the drawer (overrides showDrawerOnMinimapMouseOver)
     
     showMouseoverZoneText = true,
     alphaMouseoverZoneText = 1,
