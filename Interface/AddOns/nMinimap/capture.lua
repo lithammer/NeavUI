e679f3abfa244d59259c1a7528965253bcd1178d
Neav <anton.ickert@web.de>
2011-07-07 17:37:45 +0200

    updated and fixed capture bar

--- Interface/AddOns/nMinimap/capture.lua
+++ Interface/AddOns/nMinimap/capture.lua
@@ -6,13 +6,36 @@ hooksecurefunc('UIParent_ManageFramePositions', function()
 
             if (bar and bar:IsVisible()) then
                 bar:ClearAllPoints()
+                bar:SetScale(0.9333334)
 
                 if (i == 1) then
-                    bar:Point('TOP', Minimap, 'BOTTOM', 0, -25)
+                    bar:SetPoint('TOP', Minimap, 'BOTTOM', 0, nMinimap.positionDrawerBelow and -30 or -10)
                 else
-                    bar:Point('TOPLEFT', _G['WorldStateCaptureBar'..(i-1)], 'TOPLEFT', 0, -25)
+                    bar:SetPoint('TOP', _G['WorldStateCaptureBar'..(i-1)], 'BOTTOM', 0, -15)
                 end
             end
         end
     end
 end)
+
+WorldStateAlwaysUpFrame:ClearAllPoints()
+WorldStateAlwaysUpFrame:SetPoint('TOP', UIParent, 0, -15)
+
+--[[
+hooksecurefunc('WorldStateAlwaysUpFrame_Update', function()
+    if (NUM_ALWAYS_UP_UI_FRAMES) then
+        for i = 1, NUM_ALWAYS_UP_UI_FRAMES do
+            local frame = _G['AlwaysUpFrame'..i]
+            if (frame and frame:IsVisible()) then
+                local text = _G['AlwaysUpFrame'..i..'Text']
+                text:ClearAllPoints() 
+                text:SetPoint('CENTER', frame)
+
+                local icon = _G['AlwaysUpFrame'..i..'Icon']
+                icon:ClearAllPoints() 
+                icon:SetPoint('RIGHT', text, 'LEFT', 9, -9)
+            end
+        end
+    end
+end)
+--]]
