c392dfe7c46a9671316d8d42972ee20f2a32ca21
Peter Renström <renstrom.peter@gmail.com>
2012-07-04 23:55:28 +0200

    Hack to remove the stancebar art and resizing when only one actionbar is shown

--- Interface/AddOns/nMainbar/modules/stancebar.lua
+++ Interface/AddOns/nMainbar/modules/stancebar.lua
@@ -7,6 +7,22 @@ StanceBarFrame:SetFrameStrata('HIGH')
 StanceBarFrame:SetScale(cfg.stanceBar.scale)
 StanceBarFrame:SetAlpha(cfg.stanceBar.alpha)
 
+hooksecurefunc('UIParent_ManageFramePositions', function()
+	if (not MultiBarBottomLeft:IsShown()) then
+		SlidingActionBarTexture0:Hide()
+		SlidingActionBarTexture1:Hide()
+		if ( StanceBarFrame ) then
+			StanceBarLeft:Hide()
+			StanceBarRight:Hide()
+			StanceBarMiddle:Hide()
+			for i = 1, NUM_STANCE_SLOTS do
+				_G['StanceButton'..i]:GetNormalTexture():SetWidth(52)
+				_G['StanceButton'..i]:GetNormalTexture():SetHeight(52)
+			end
+		end
+	end
+end)
+
 if (cfg.stanceBar.hide) then
     for i = 1, NUM_STANCE_SLOTS do
         local button = _G['StanceButton'..i]
