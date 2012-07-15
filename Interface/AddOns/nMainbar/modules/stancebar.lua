0fba1d055cc4d45e32a8e081582e8d35725ca95d
renstrom <renstrom.peter@gmail.com>
2012-07-15 14:28:28 +0200

    Override StanceBar_Update Only reason is because in Blizzard's original function they reposition StanceButton1 when GetNumShapeShiftForms() == 1 which makes it impossible to have a custom position persist.

--- Interface/AddOns/nMainbar/modules/stancebar.lua
+++ Interface/AddOns/nMainbar/modules/stancebar.lua
@@ -7,22 +7,6 @@ StanceBarFrame:SetFrameStrata('HIGH')
 StanceBarFrame:SetScale(cfg.stanceBar.scale)
 StanceBarFrame:SetAlpha(cfg.stanceBar.alpha)
 
-hooksecurefunc('UIParent_ManageFramePositions', function()
-	if (not MultiBarBottomLeft:IsShown()) then
-		SlidingActionBarTexture0:Hide()
-		SlidingActionBarTexture1:Hide()
-		if (StanceBarFrame) then
-			StanceBarLeft:Hide()
-			StanceBarRight:Hide()
-			StanceBarMiddle:Hide()
-			for i = 1, NUM_STANCE_SLOTS do
-				_G['StanceButton'..i]:GetNormalTexture():SetWidth(52)
-				_G['StanceButton'..i]:GetNormalTexture():SetHeight(52)
-			end
-		end
-	end
-end)
-
 if (cfg.stanceBar.hide) then
     for i = 1, NUM_STANCE_SLOTS do
         local button = _G['StanceButton'..i]
@@ -32,4 +16,29 @@ if (cfg.stanceBar.hide) then
         button:EnableMouse(false)
         button.EnableMouse = function() end
     end
+end
+
+hooksecurefunc('UIParent_ManageFramePositions', function()
+    if (StanceBarFrame) then
+        StanceBarLeft:Hide()
+        StanceBarRight:Hide()
+        StanceBarMiddle:Hide()
+        for i = 1, NUM_STANCE_SLOTS do
+            _G['StanceButton'..i]:GetNormalTexture():SetWidth(52)
+            _G['StanceButton'..i]:GetNormalTexture():SetHeight(52)
+        end
+    end
+end)
+
+-- Override Blizzard's function because they keep re-positioning the stance bar
+-- when GetNumShapeshiftForms() == 1
+function StanceBar_Update()
+    local numForms = GetNumShapeshiftForms()
+    if (numForms > 0 and not IsPossessBarVisible()) then
+        StanceBarFrame:Show()
+        StanceBar_UpdateState()
+    else
+        StanceBarFrame:Hide()
+    end
+    UIParent_ManageFramePositions()
 end
