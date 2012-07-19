504a7b556d3cce9f61bf0908f4b5d13a5fb6677b
renstrom <renstrom.peter@gmail.com>
2012-07-19 20:47:10 +0200

    Reworked the StanceBar hack again.

--- Interface/AddOns/nMainbar/modules/short/extras.lua
+++ Interface/AddOns/nMainbar/modules/short/extras.lua
@@ -8,7 +8,7 @@ end
 
     -- moveable bars
 
-for _, frame in pairs({        
+for _, frame in pairs({
     _G['PetActionBarFrame'],
     _G['StanceBarFrame'],
     _G['PossessBarFrame'],
@@ -19,10 +19,11 @@ end
 
     -- key + alt-key and left mouse to move
 
-for _, button in pairs({        
+for _, button in pairs({
     _G['PossessButton1'],
     _G['PetActionButton1'],
     _G['StanceButton1'],
+    _G['StanceBarFrame'],
 }) do
     button:ClearAllPoints()
     button:SetPoint('CENTER', UIParent, -100)
@@ -33,11 +34,22 @@ for _, button in pairs({
     button:RegisterForDrag('LeftButton')
     button:HookScript('OnDragStart', function(self)
         if (IsShiftKeyDown() and IsAltKeyDown()) then
-            self:StartMoving() 
+            self:StartMoving()
         end
     end)
 
     button:HookScript('OnDragStop', function(self) 
         self:StopMovingOrSizing()
+    end)
+end
+
+-- HACK: This will prevent Blizzard's StanceBar_Update() function from
+-- re-positioning StanceButton1 when GetNumShapeshiftForms() == 1.
+if (GetNumShapeshiftForms() == 1) then
+    StanceButton1:HookScript('OnDragStop', function(self)
+        local point, relativeTo, relativePoint, xOffset, yOffset = self:GetPoint()
+        StanceBarFrame:ClearAllPoints()
+        -- 12 and 3 is to offset for StanceButton1's relative position towards StanceBarFrame
+        StanceBarFrame:SetPoint(point, relativeTo, relativePoint, xOffset - 12, yOffset - 3)
     end)
 end
