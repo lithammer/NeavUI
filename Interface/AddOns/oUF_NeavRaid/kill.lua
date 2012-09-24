4d9873184cf5f576a19017cd11fd8795bbb02cbc
Peter Renström <renstrom.peter@gmail.com>
2012-09-24 20:27:04 +0200

    oUF_NeavRaid: Reworked the way to kill Blizzard's raid frames.
    Might or might not decrease taint.

--- Interface/AddOns/oUF_NeavRaid/kill.lua
+++ Interface/AddOns/oUF_NeavRaid/kill.lua
@@ -2,49 +2,25 @@
 InterfaceOptionsFrameCategoriesButton11:SetScale(0.00001)
 InterfaceOptionsFrameCategoriesButton11:SetAlpha(0)
 
---[[
-function CompactUnitFrame_UpdateVisible() end
-function CompactUnitFrame_UpdateAll() end
+CompactRaidFrameManager:ClearAllPoints()
+CompactRaidFrameManager:SetPoint('TOP', UIParent, 'BOTTOM', 0, -9000)
+CompactUnitFrameProfiles:UnregisterAllEvents()
 
-function CompactRaidFrameContainer_OnLoad() end
-function CompactUnitFrame_SetUnit() end
+for i = 1, MAX_PARTY_MEMBERS do
+    local name = 'PartyMemberFrame'..i
+    local frame = _G[name]
 
-function CompactRaidFrameManager_OnEvent() end
-function CompactRaidFrameManager_SetBorderShown() end
-function CompactRaidFrameManager_SetSetting() end
-function CompactRaidFrameManager_SetManaged() end
+    frame:ClearAllPoints()
+    frame:SetPoint('TOP', UIParent, 'BOTTOM', 0, -9000)
 
-local function KillRaidFrame()
-    CompactRaidFrameManager:UnregisterAllEvents()
-    if (not InCombatLockdown()) then 
-        CompactRaidFrameManager:Hide() 
-    end
+    _G[name..'HealthBar']:UnregisterAllEvents()
+    _G[name..'ManaBar']:UnregisterAllEvents()
 
-    local shown = CompactRaidFrameManager_GetSetting('IsShown')
-    if (shown and shown ~= '0') then
-        CompactRaidFrameManager_SetSetting('IsShown', '0')
-    end
-end
+    local pet = name..'PetFrame'
+    local petframe = _G[pet]
 
-hooksecurefunc('CompactRaidFrameManager_UpdateShown', function()
-    KillRaidFrame()
-end)
+    petframe:ClearAllPoints()
+    petframe:SetPoint('TOP', UIParent, 'BOTTOM', 0, -9000)
 
-KillRaidFrame()
---]]
-
-function CompactUnitFrame_UpdateVisible() end
-function CompactUnitFrame_UpdateAll() end
-function CompactRaidFrameContainer_OnLoad() end
-
-for _, object in pairs({
-    CompactPartyFrame,
-    CompactRaidFrameManager,
-    CompactRaidFrameContainer,
-}) do
-    object:UnregisterAllEvents()
-
-    hooksecurefunc(object, 'Show', function(self)
-        self:Hide()
-    end)
+    _G[pet]:UnregisterAllEvents()
 end
