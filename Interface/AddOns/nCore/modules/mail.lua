3d44ba6162ba4043d5d508acc6c865e207f54399
Peter Renström <renstrom.peter@gmail.com>
2012-10-05 12:11:04 +0200

    nCore: Print total money collected from mailbox.

--- Interface/AddOns/nCore/modules/mail.lua
+++ Interface/AddOns/nCore/modules/mail.lua
@@ -6,31 +6,38 @@ f:SetText(OPENMAIL)
 
 local processing = false
 local function OnEvent()
-	if (not MailFrame:IsShown()) then 
+    if (not MailFrame:IsShown()) then
         return 
     end
 
-	local num = GetInboxNumItems()
-	if (processing) then
-		if (num == 0) then
-			MiniMapMailFrame:Hide()
-			processing = false
-			return
-		end
+    local num = GetInboxNumItems()
+    if (processing) then
+        if (num == 0) then
+            MiniMapMailFrame:Hide()
+            processing = false
+            return
+        end
 
-		for i = num, 1, -1 do
-			local _, _, _, _, money, COD, _, item = GetInboxHeaderInfo(i)
-			if (item and COD < 1) then
-				TakeInboxItem(i)
-				return
-			end
+        local totalMoney = 0
+        for i = num, 1, -1 do
+            local _, _, _, _, money, COD, _, item = GetInboxHeaderInfo(i)
+            if (item and COD < 1) then
+                TakeInboxItem(i)
+                return
+            end
 
-			if (money > 0) then
-				TakeInboxMoney(i)
-				return
-			end
-		end
-	end
+            if (money > 0) then
+                totalMoney = totalMoney + money
+                TakeInboxMoney(i)
+                return
+            end
+        end
+
+        if (totalMoney > 0) then
+            local chatWindowFontSize = select(2, GetChatWindowInfo(1))
+            DEFAULT_CHAT_FRAME:AddMessage('Total money collected from mailbox was '..GetCoinTextureString(totalMoney, chatWindowFontSize)..'.')
+        end
+    end
 end
 
 f:RegisterEvent('MAIL_INBOX_UPDATE')
