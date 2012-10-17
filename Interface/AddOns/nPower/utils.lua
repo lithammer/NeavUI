3a081d72325df95d914acdd63a1f5522a35f4fd4
Peter Renström <renstrom.peter@gmail.com>
2012-10-17 14:05:43 +0200

    nPower: Config variable name consistency.

--- Interface/AddOns/nPower/utils.lua
+++ Interface/AddOns/nPower/utils.lua
@@ -1,10 +1,10 @@
 
-local _, addon = ...
+local _, nPower = ...
 
 local format = string.format
 local floor = math.floor
 
-function addon:FormatValue(self)
+function nPower:FormatValue(self)
     if (self >= 10000) then
         return ('%.1fk'):format(self / 1e3)
     else
@@ -12,12 +12,12 @@ function addon:FormatValue(self)
     end
 end
 
-function addon:Round(num, idp)
+function nPower:Round(num, idp)
 	local mult = 10^(idp or 0)
 	return floor(num * mult + 0.5) / mult
 end
 
-function addon:Fade(frame, timeToFade, startAlpha, endAlpha)
+function nPower:Fade(frame, timeToFade, startAlpha, endAlpha)
 	if (self:Round(frame:GetAlpha(), 1) ~= endAlpha) then
 		local mode = startAlpha > endAlpha and 'In' or 'Out'
         securecall('UIFrameFade'..mode, frame, timeToFade, startAlpha, endAlpha)
