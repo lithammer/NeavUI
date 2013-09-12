558f70f393eed15426cd4d51fd096740d72e0ff4
Peter Renström <peter.renstrom@egbs.se>
2013-09-12 16:56:54 +0200

    [oUF_NeavRaid] Add Siege of Orgrimmar raid debuffs.

--- Interface/AddOns/oUF_NeavRaid/modules/oUF_Freebgrid/oUF_Freebgrid_AurasList.lua
+++ Interface/AddOns/oUF_NeavRaid/modules/oUF_Freebgrid/oUF_Freebgrid_AurasList.lua
@@ -23,6 +23,7 @@ end
     -- Note: Just must be in this instance, when you run the script above
 
 local L = {
+    ['Siege of Orgrimmar'] = 953,
     ['Throne of Thunder'] = 930,
     ['Terrace of Endless Spring'] = 886,
     ['Heart of Fear'] = 897,
@@ -85,6 +86,176 @@ ns.auras = {
         -- Raid Debuffs
 
     instances = {
+        [L['Siege of Orgrimmar']] = {
+
+                -- Immerseus
+
+            [GetSpellInfo(143297)] = 5, -- Sha Splash
+            [GetSpellInfo(143459)] = 4, -- Sha Residue
+            [GetSpellInfo(143524)] = 4, -- Purified Residue
+            [GetSpellInfo(143286)] = 4, -- Seeping Sha
+            [GetSpellInfo(143413)] = 3, -- Swirl
+            [GetSpellInfo(143411)] = 3, -- Swirl
+            [GetSpellInfo(143436)] = 2, -- Corrosive Blast (tanks)
+            [GetSpellInfo(143579)] = 3, -- Sha Corruption (Heroic Only)
+
+                -- Fallen Protectors
+
+            [GetSpellInfo(143239)] = 4, -- Noxious Poison
+            [GetSpellInfo(144176)] = 2, -- Lingering Anguish
+            [GetSpellInfo(143023)] = 3, -- Corrupted Brew
+            [GetSpellInfo(143301)] = 2, -- Gouge
+            [GetSpellInfo(143564)] = 3, -- Meditative Field
+            [GetSpellInfo(143010)] = 3, -- Corruptive Kick
+            [GetSpellInfo(143434)] = 6, -- Shadow Word:Bane (Dispell)
+            [GetSpellInfo(143840)] = 6, -- Mark of Anguish
+            [GetSpellInfo(143959)] = 4, -- Defiled Ground
+            [GetSpellInfo(143423)] = 6, -- Sha Sear
+            [GetSpellInfo(143292)] = 5, -- Fixate
+            [GetSpellInfo(144176)] = 5, -- Shadow Weakness
+            [GetSpellInfo(147383)] = 4, -- Debilitation (Heroic Only)
+
+                -- Norushen
+
+            [GetSpellInfo(146124)] = 2, -- Self Doubt (tanks)
+            [GetSpellInfo(146324)] = 4, -- Jealousy
+            [GetSpellInfo(144639)] = 6, -- Corruption
+            [GetSpellInfo(144850)] = 5, -- Test of Reliance
+            [GetSpellInfo(145861)] = 6, -- Self-Absorbed (Dispell)
+            [GetSpellInfo(144851)] = 2, -- Test of Confiidence (tanks)
+            [GetSpellInfo(146703)] = 3, -- Bottomless Pit
+            [GetSpellInfo(144514)] = 6, -- Lingering Corruption
+            [GetSpellInfo(144849)] = 4, -- Test of Serenity
+
+                -- Sha of Pride
+
+            [GetSpellInfo(144358)] = 2, -- Wounded Pride (tanks)
+            [GetSpellInfo(144843)] = 3, -- Overcome
+            [GetSpellInfo(146594)] = 4, -- Gift of the Titans
+            [GetSpellInfo(144351)] = 6, -- Mark of Arrogance
+            [GetSpellInfo(144364)] = 4, -- Power of the Titans
+            [GetSpellInfo(146822)] = 6, -- Projection
+            [GetSpellInfo(146817)] = 5, -- Aura of Pride
+            [GetSpellInfo(144774)] = 2, -- Reaching Attacks (tanks)
+            [GetSpellInfo(144636)] = 5, -- Corrupted Prison
+            [GetSpellInfo(144574)] = 6, -- Corrupted Prison
+            [GetSpellInfo(145215)] = 4, -- Banishment (Heroic)
+            [GetSpellInfo(147207)] = 4, -- Weakened Resolve (Heroic)
+            [GetSpellInfo(144574)] = 6, -- Corrupted Prison
+            [GetSpellInfo(144574)] = 6, -- Corrupted Prison
+
+                -- Galakras
+
+            [GetSpellInfo(146765)] = 5, -- Flame Arrows
+            [GetSpellInfo(147705)] = 5, -- Poison Cloud
+            [GetSpellInfo(146902)] = 2, -- Poison Tipped blades
+
+                -- Iron Juggernaut
+
+            [GetSpellInfo(144467)] = 2, -- Ignite Armor
+            [GetSpellInfo(144459)] = 5, -- Laser Burn
+            [GetSpellInfo(144498)] = 5, -- Napalm Oil
+            [GetSpellInfo(144918)] = 5, -- Cutter Laser
+            [GetSpellInfo(146325)] = 6, -- Cutter Laser Target
+
+                -- Kor'kron Dark Shaman
+
+            [GetSpellInfo(144089)] = 6, -- Toxic Mist
+            [GetSpellInfo(144215)] = 2, -- Froststorm Strike (Tank only)
+            [GetSpellInfo(143990)] = 2, -- Foul Geyser (Tank only)
+            [GetSpellInfo(144304)] = 2, -- Rend
+            [GetSpellInfo(144330)] = 6, -- Iron Prison (Heroic)
+
+                -- General Nazgrim
+
+            [GetSpellInfo(143638)] = 6, -- Bonecracker
+            [GetSpellInfo(143480)] = 5, -- Assassin's Mark
+            [GetSpellInfo(143431)] = 6, -- Magistrike (Dispell)
+            [GetSpellInfo(143494)] = 2, -- Sundering Blow (Tanks)
+            [GetSpellInfo(143882)] = 5, -- Hunter's Mark
+
+                -- Malkorok
+
+            [GetSpellInfo(142990)] = 2, -- Fatal Strike (Tank debuff)
+            [GetSpellInfo(142913)] = 6, -- Displaced Energy (Dispell)
+            [GetSpellInfo(143919)] = 5, -- Languish (Heroic)
+
+                -- Spoils of Pandaria
+
+            [GetSpellInfo(145685)] = 2, -- Unstable Defensive System
+            [GetSpellInfo(144853)] = 3, -- Carnivorous Bite
+            [GetSpellInfo(145987)] = 5, -- Set to Blow
+            [GetSpellInfo(145218)] = 4, -- Harden Flesh
+            [GetSpellInfo(145230)] = 1, -- Forbidden Magic
+            [GetSpellInfo(146217)] = 4, -- Keg Toss
+            [GetSpellInfo(146235)] = 4, -- Breath of Fire
+            [GetSpellInfo(145523)] = 4, -- Animated Strike
+            [GetSpellInfo(142983)] = 6, -- Torment (the new Wrack)
+            [GetSpellInfo(145715)] = 3, -- Blazing Charge
+            [GetSpellInfo(145747)] = 5, -- Bubbling Amber
+            [GetSpellInfo(146289)] = 4, -- Mass Paralysis
+
+                -- Thok the Bloodthirsty
+
+            [GetSpellInfo(143766)] = 2, -- Panic (tanks)
+            [GetSpellInfo(143773)] = 2, -- Freezing Breath (tanks)
+            [GetSpellInfo(143452)] = 1, -- Bloodied
+            [GetSpellInfo(146589)] = 5, -- Skeleton Key (tanks)
+            [GetSpellInfo(143445)] = 6, -- Fixate
+            [GetSpellInfo(143791)] = 5, -- Corrosive Blood
+            [GetSpellInfo(143777)] = 3, -- Frozen Solid (tanks)
+            [GetSpellInfo(143780)] = 4, -- Acid Breath
+            [GetSpellInfo(143800)] = 5, -- Icy Blood
+            [GetSpellInfo(143428)] = 4, -- Tail Lash
+
+                -- Siegecrafter Blackfuse
+
+            [GetSpellInfo(144236)] = 4, -- Pattern Recognition
+            [GetSpellInfo(144466)] = 5, -- Magnetic Crush
+            [GetSpellInfo(143385)] = 2, -- Electrostatic Charge (tank)
+            [GetSpellInfo(143856)] = 6, -- Superheated
+
+                -- Paragons of the Klaxxi
+
+            [GetSpellInfo(143617)] = 5, -- Blue Bomb
+            [GetSpellInfo(143701)] = 5, -- Whirling (stun)
+            [GetSpellInfo(143702)] = 5, -- Whirling
+            [GetSpellInfo(142808)] = 6, -- Fiery Edge
+            [GetSpellInfo(143609)] = 5, -- Yellow Sword
+            [GetSpellInfo(143610)] = 5, -- Red Drum
+            [GetSpellInfo(142931)] = 2, -- Exposed Veins
+            [GetSpellInfo(143619)] = 5, -- Yellow Bomb
+            [GetSpellInfo(143735)] = 6, -- Caustic Amber
+            [GetSpellInfo(146452)] = 5, -- Resonating Amber
+            [GetSpellInfo(142929)] = 2, -- Tenderizing Strikes
+            [GetSpellInfo(142797)] = 5, -- Noxious Vapors
+            [GetSpellInfo(143939)] = 5, -- Gouge
+            [GetSpellInfo(143275)] = 2, -- Hewn
+            [GetSpellInfo(143768)] = 2, -- Sonic Projection
+            [GetSpellInfo(142532)] = 6, -- Toxin: Blue
+            [GetSpellInfo(142534)] = 6, -- Toxin: Yellow
+            [GetSpellInfo(143279)] = 2, -- Genetic Alteration
+            [GetSpellInfo(143339)] = 6, -- Injection
+            [GetSpellInfo(142649)] = 4, -- Devour
+            [GetSpellInfo(146556)] = 6, -- Pierce
+            [GetSpellInfo(142671)] = 6, -- Mesmerize
+            [GetSpellInfo(143979)] = 2, -- Vicious Assault
+            [GetSpellInfo(143607)] = 5, -- Blue Sword
+            [GetSpellInfo(143614)] = 5, -- Yellow Drum
+            [GetSpellInfo(143612)] = 5, -- Blue Drum
+            [GetSpellInfo(142533)] = 6, -- Toxin: Red
+            [GetSpellInfo(143615)] = 5, -- Red Bomb
+            [GetSpellInfo(143974)] = 2, -- Shield Bash (tanks)
+
+                -- Garrosh Hellscream
+
+            [GetSpellInfo(144582)] = 4, -- Hamstring
+            [GetSpellInfo(144954)] = 4, -- Realm of Y'Shaarj
+            [GetSpellInfo(145183)] = 2, -- Gripping Despair (tanks)
+            [GetSpellInfo(144762)] = 4, -- Desecrated
+            [GetSpellInfo(145071)] = 5, -- Touch of Y'Sharrj
+            [GetSpellInfo(148718)] = 4, -- Fire Pit
+        },
         [L['Throne of Thunder']] = {
 
                 -- Jin'rokh the Breaker
@@ -98,17 +269,17 @@ ns.auras = {
                 -- Horridon
 
             [GetSpellInfo(136769)] = 6, -- Charge
-            [GetSpellInfo(136767)] = 6, -- Triple Puncture (tanks only)
-            [GetSpellInfo(136708)] = 3, -- Stone Gaze
+            [GetSpellInfo(136767)] = 2, -- Triple Puncture (tanks only)
+            [GetSpellInfo(136708)] = 6, -- Stone Gaze
             [GetSpellInfo(136723)] = 5, -- Sand Trap
             [GetSpellInfo(136587)] = 5, -- Venom Bolt Volley (dispellable)
             [GetSpellInfo(136710)] = 5, -- Deadly Plague (disease)
             [GetSpellInfo(136670)] = 4, -- Mortal Strike
-            [GetSpellInfo(136573)] = 6, -- Frozen Bolt (Debuff used by frozen orb)
-            [GetSpellInfo(136512)] = 5, -- Hex of Confusion
+            [GetSpellInfo(136573)] = 5, -- Frozen Bolt (Debuff used by frozen orb)
+            [GetSpellInfo(136512)] = 6, -- Hex of Confusion
             [GetSpellInfo(136719)] = 6, -- Blazing Sunlight
-            [GetSpellInfo(136654)] = 4, -- Rending Charge
-            [GetSpellInfo(140946)] = 7, -- Dire Fixation (Heroic Only)
+            [GetSpellInfo(136654)] = 6, -- Rending Charge
+            [GetSpellInfo(140946)] = 4, -- Dire Fixation (Heroic Only)
 
                 -- Council of Elders
 
@@ -153,18 +324,18 @@ ns.auras = {
 
             [GetSpellInfo(133768)] = 2, -- Arterial Cut (tank only)
             [GetSpellInfo(133767)] = 2, -- Serious Wound (Tank only)
-            [GetSpellInfo(136932)] = 7, -- Force of Will
-            [GetSpellInfo(134122)] = 6, -- Blue Beam
-            [GetSpellInfo(134123)] = 6, -- Red Beam
-            [GetSpellInfo(134124)] = 6, -- Yellow Beam
-            [GetSpellInfo(133795)] = 4, -- Life Drain
+            [GetSpellInfo(136932)] = 6, -- Force of Will
+            [GetSpellInfo(134122)] = 5, -- Blue Beam
+            [GetSpellInfo(134123)] = 5, -- Red Beam
+            [GetSpellInfo(134124)] = 5, -- Yellow Beam
+            [GetSpellInfo(133795)] = 6, -- Life Drain
             [GetSpellInfo(133597)] = 6, -- Dark Parasite
-            [GetSpellInfo(133732)] = 5, -- Infrared Light (the stacking red debuff)
-            [GetSpellInfo(133677)] = 5, -- Blue Rays (the stacking blue debuff)
-            [GetSpellInfo(133738)] = 5, -- Bright Light (the stacking yellow debuff)
-            [GetSpellInfo(133737)] = 6, -- Bright Light (The one that says you are actually in a beam)
-            [GetSpellInfo(133675)] = 6, -- Blue Rays (The one that says you are actually in a beam)
-            [GetSpellInfo(134626)] = 6, -- Lingering Gaze
+            [GetSpellInfo(133732)] = 3, -- Infrared Light (the stacking red debuff)
+            [GetSpellInfo(133677)] = 3, -- Blue Rays (the stacking blue debuff)
+            [GetSpellInfo(133738)] = 3, -- Bright Light (the stacking yellow debuff)
+            [GetSpellInfo(133737)] = 4, -- Bright Light (The one that says you are actually in a beam)
+            [GetSpellInfo(133675)] = 4, -- Blue Rays (The one that says you are actually in a beam)
+            [GetSpellInfo(134626)] = 5, -- Lingering Gaze
 
                 -- Primordius
 
@@ -201,6 +372,7 @@ ns.auras = {
             [GetSpellInfo(137654)] = 5, -- Rushing Winds
             [GetSpellInfo(136577)] = 4, -- Wind Storm
             [GetSpellInfo(136192)] = 4, -- Lightning Storm
+            [GetSpellInfo(136615)] = 6, -- Electrified
 
                 -- Twin Consorts
 
@@ -218,16 +390,19 @@ ns.auras = {
             [GetSpellInfo(136295)] = 6, -- Overcharged
             [GetSpellInfo(135000)] = 2, -- Decapitate (Tank only)
             [GetSpellInfo(136478)] = 5, -- Fusion Slash
-            [GetSpellInfo(136543)] = 6, -- Ball Lightning
-            [GetSpellInfo(134821)] = 6, -- Discharged Energy
+            [GetSpellInfo(136543)] = 5, -- Ball Lightning
+            [GetSpellInfo(134821)] = 4, -- Discharged Energy
             [GetSpellInfo(136326)] = 6, -- Overcharge
-            [GetSpellInfo(137176)] = 6, -- Overloaded Circuits
-            [GetSpellInfo(136853)] = 6, -- Lightning Bolt
+            [GetSpellInfo(137176)] = 4, -- Overloaded Circuits
+            [GetSpellInfo(136853)] = 4, -- Lightning Bolt
             [GetSpellInfo(135153)] = 6, -- Crashing Thunder
             [GetSpellInfo(136914)] = 2, -- Electrical Shock
             [GetSpellInfo(135001)] = 2, -- Maim
 
-            -- Ra-Den (Heroic only)
+                -- Ra-Den (Heroic only)
+
+            [GetSpellInfo(138308)] = 6, --Unstable Vita
+            [GetSpellInfo(138372)] = 5, --Vita Sensitivity
         },
 
         [L['Terrace of Endless Spring']] = {
