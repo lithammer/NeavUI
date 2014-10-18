4cda6ec23f4a03e58fc77c3de64d579e38d26322
Peter Renström <renstrom.peter@gmail.com>
2014-10-18 13:59:52 +0200

    [nCore] Make objective tracker movable. Fix #47.

---
+++ Interface/AddOns/nCore/modules/objectivetracker.lua
@@ -0,0 +1,34 @@
+local objectiveTrackerFrame = _G['ObjectiveTrackerFrame']
+objectiveTrackerFrame.ClearAllPoints = function() end
+objectiveTrackerFrame:SetClampedToScreen(true)
+objectiveTrackerFrame:SetMovable(true)
+objectiveTrackerFrame:SetUserPlaced(true)
+objectiveTrackerFrame.SetPoint = function() end
+
+local minimizeButton = objectiveTrackerFrame.HeaderMenu.MinimizeButton
+
+minimizeButton:EnableMouse(true)
+minimizeButton:RegisterForDrag('LeftButton')
+minimizeButton:SetHitRectInsets(-15, 0, -5, -5)
+minimizeButton:SetScript('OnDragStart', function(self) 
+    if (IsShiftKeyDown()) then
+        objectiveTrackerFrame:StartMoving()
+    end
+end)
+
+minimizeButton:SetScript('OnDragStop', function(self) 
+    objectiveTrackerFrame:StopMovingOrSizing()
+end)
+
+minimizeButton:SetScript('OnEnter', function()
+    if (not InCombatLockdown()) then
+	GameTooltip:SetOwner(minimizeButton, 'ANCHOR_TOPLEFT', 0, 10)
+	GameTooltip:ClearLines()
+	GameTooltip:AddLine('Shift + left-click to Drag')
+        GameTooltip:Show()
+    end
+end)
+
+minimizeButton:SetScript('OnLeave', function()
+    GameTooltip:Hide()
+end)
