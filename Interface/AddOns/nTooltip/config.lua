e767c7df27c72e23202d73043977d11c88661694
Peter Renström <renstrom.peter@gmail.com>
2014-11-12 21:00:39 +0100

    [nTooltip] Revert accidental config change.
    Accidentally pushed a config change while trying to fix a bug.

--- Interface/AddOns/nTooltip/config.lua
+++ Interface/AddOns/nTooltip/config.lua
@@ -16,7 +16,7 @@ nTooltip.Config = {
     reactionBorderColor = false,
     itemqualityBorderColor = true,
 
-    abbrevRealmNames = false, 
+    abbrevRealmNames = false,
     hideRealmText = false,                      -- Hide the coalesced/interactive realm text
     showPlayerTitles = true,
     showUnitRole = true,
@@ -29,19 +29,19 @@ nTooltip.Config = {
         showHealthValue = true,
 
         healthFormat = '$cur / $max',           -- Possible: $cur, $max, $deficit, $perc, $smartperc, $smartcolorperc, $colorperc
-        healthFullFormat = '$cur',              -- if the tooltip unit has 100% hp 
+        healthFullFormat = '$cur',              -- if the tooltip unit has 100% hp
 
         fontSize = 13,
         font = 'Fonts\\ARIALN.ttf',
         showOutline = true,
         textPos = 'CENTER',                     -- Possible 'TOP' 'BOTTOM' 'CENTER'
 
-        reactionColoring = true,               -- Overrides customColor 
+        reactionColoring = false,               -- Overrides customColor
         customColor = {
-            apply = false, 
-            r = 0, 
-            g = 1, 
+            apply = false,
+            r = 0,
+            g = 1,
             b = 1
-        } 
+        }
     },
 }
