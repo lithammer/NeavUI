11393283a7f4c4488c81c886b208b00e5b12f41c
Peter Renström <renstrom.peter@gmail.com>
2014-12-14 13:17:09 +0100

    [oUF_NeavRaid] Add healing indicator for Germination. Fixes #61.

--- Interface/AddOns/oUF_NeavRaid/core.lua
+++ Interface/AddOns/oUF_NeavRaid/core.lua
@@ -13,6 +13,7 @@ do
         DRUID = {
             {774, 'BOTTOMRIGHT', {1, 0.2, 1}}, -- Rejuvenation
             {33763, 'BOTTOM', {0.5, 1, 0.5}, false, false, true}, -- Lifebloom
+            {155777, 'RIGHT', {0.4, 0.9, 0.4}}, -- Rejuvenation (Germination)
             {48438, 'BOTTOMLEFT', {0.7, 1, 0}}, -- Wild Growth
         },
         MONK = {
