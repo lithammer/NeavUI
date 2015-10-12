bae4c64f945727e14169f2bf275a690ee1c10149
Peter Renström <renstrom.peter@gmail.com>
2015-10-12 19:27:02 +0200

    tooltip: Don't show spellid twice on talent frame
    Fixes #76

--- Interface/AddOns/nCore/modules/spellid.lua
+++ Interface/AddOns/nCore/modules/spellid.lua
@@ -1,6 +1,6 @@
 
 hooksecurefunc(GameTooltip, 'SetUnitBuff', function(self,...)
-    local id = select(11,UnitBuff(...))
+    local id = select(11, UnitBuff(...))
     if (id) then
         self:AddLine('SpellID: '..id, 1, 1, 1)
         self:Show()
@@ -8,7 +8,7 @@ hooksecurefunc(GameTooltip, 'SetUnitBuff', function(self,...)
 end)
 
 hooksecurefunc(GameTooltip, 'SetUnitDebuff', function(self,...)
-    local id = select(11,UnitDebuff(...))
+    local id = select(11, UnitDebuff(...))
     if (id) then
         self:AddLine('SpellID: '..id, 1, 1, 1)
         self:Show()
@@ -25,15 +25,23 @@ end)
 
 hooksecurefunc('SetItemRef', function(link, text, button, chatFrame)
     if (string.find(link,'^spell:')) then
-        local id = string.sub(link,7)
+        local id = string.sub(link, 7)
         ItemRefTooltip:AddLine('SpellID: '..id, 1, 1, 1)
         ItemRefTooltip:Show()
     end
 end)
 
 GameTooltip:HookScript('OnTooltipSetSpell', function(self)
-    local id = select(3,self:GetSpell())
+    local id = select(3, self:GetSpell())
     if (id) then
+        -- Workaround for weird issue when this gets triggered twice on the Talents frame
+        -- https://github.com/renstrom/NeavUI/issues/76
+        for i = 1, self:NumLines() do
+            if _G['GameTooltipTextLeft'..i]:GetText() == 'SpellID: '..id then
+                return
+            end
+        end
+
         self:AddLine('SpellID: '..id, 1, 1, 1)
         self:Show()
     end
