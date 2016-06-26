fa575f53b6eb2a5354b517fe52faa9284166984a
Thomas Shannon <thomasjohnshannon@gmail.com>
2016-06-26 15:44:34 -0600

    Interface Option Changed
    Hide that code for the time being.

--- Interface/AddOns/oUF_NeavRaid/kill.lua
+++ Interface/AddOns/oUF_NeavRaid/kill.lua
@@ -2,8 +2,8 @@
 local frameHider = CreateFrame('Frame')
 frameHider:Hide()
 
-InterfaceOptionsFrameCategoriesButton11:SetScale(0.00001)
-InterfaceOptionsFrameCategoriesButton11:SetAlpha(0)
+--InterfaceOptionsFrameCategoriesButton11:SetScale(0.00001)
+--InterfaceOptionsFrameCategoriesButton11:SetAlpha(0)
 
 CompactRaidFrameManager:SetParent(frameHider)
 CompactUnitFrameProfiles:UnregisterAllEvents()
