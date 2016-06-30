121a1a0b5167a7113ba8fe9497d463783989babf
Peter Renström <renstrom.peter@gmail.com>
2016-06-30 15:47:13 +0200

    nMainbar: Correctly hide hotkeys from petbar
    Fix #80

--- Interface/AddOns/nMainbar/modules/petbar.lua
+++ Interface/AddOns/nMainbar/modules/petbar.lua
@@ -11,8 +11,22 @@ PetActionBarFrame:SetAlpha(cfg.petBar.alpha)
 
 if (cfg.petBar.vertical) then
     for i = 2, 10 do
-        button = _G['PetActionButton'..i]
+        local button = _G['PetActionButton'..i]
         button:ClearAllPoints()
         button:SetPoint('TOP', _G['PetActionButton'..(i - 1)], 'BOTTOM', 0, -8)
+    end
+end
+
+hooksecurefunc('PetActionButton_SetHotkeys', function(self)
+    local hotkey = _G[self:GetName()..'HotKey']
+    if (not cfg.button.showKeybinds) then
+        hotkey:Hide()
+    end
+end)
+
+if (not cfg.button.showKeybinds) then
+    for i = 1, NUM_PET_ACTION_SLOTS, 1 do
+        local buttonName = _G['PetActionButton'..i]
+        PetActionButton_SetHotkeys(buttonName)
     end
 end
