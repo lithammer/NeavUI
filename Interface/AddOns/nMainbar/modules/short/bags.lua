5a20de5572d799a30ece160f0ab5a5f5fc677d07
Thomas Shannon <thomasjohnshannon@gmail.com>
2016-07-07 14:42:22 -0600

    Fix Spacing

--- Interface/AddOns/nMainbar/modules/short/bags.lua
+++ Interface/AddOns/nMainbar/modules/short/bags.lua
@@ -12,7 +12,7 @@ BINDING_NAME_NBAGS_TOGGLE = "Toggle Bags"
 
 -- Saved Variable Setup
 if shown == nil then
-	shown = false
+    shown = false
 end
 
 -- Clear frame position.
@@ -31,50 +31,50 @@ CharacterBag3Slot:SetPoint('RIGHT', CharacterBag2Slot, 'LEFT',0, 0)
 
 -- Set !BeautyBorder Settings
 if IsAddOnLoaded('!Beautycase') then
-	MainMenuBarBackpackButton:CreateBeautyBorder(8)
-	CharacterBag0Slot:CreateBeautyBorder(8)
-	CharacterBag1Slot:CreateBeautyBorder(8)
-	CharacterBag2Slot:CreateBeautyBorder(8)
-	CharacterBag3Slot:CreateBeautyBorder(8)
+    MainMenuBarBackpackButton:CreateBeautyBorder(8)
+    CharacterBag0Slot:CreateBeautyBorder(8)
+    CharacterBag1Slot:CreateBeautyBorder(8)
+    CharacterBag2Slot:CreateBeautyBorder(8)
+    CharacterBag3Slot:CreateBeautyBorder(8)
 end
 
 -- Show Bags
 local function BagShow()
-	MainMenuBarBackpackButton:Show()
-	CharacterBag0Slot:Show()
-	CharacterBag1Slot:Show()
-	CharacterBag2Slot:Show()
-	CharacterBag3Slot:Show()
+    MainMenuBarBackpackButton:Show()
+    CharacterBag0Slot:Show()
+    CharacterBag1Slot:Show()
+    CharacterBag2Slot:Show()
+    CharacterBag3Slot:Show()
 end
 
 -- Hide Bags
 local function BagHide()
-	MainMenuBarBackpackButton:Hide()
-	CharacterBag0Slot:Hide()
-	CharacterBag1Slot:Hide()
-	CharacterBag2Slot:Hide()
-	CharacterBag3Slot:Hide()
+    MainMenuBarBackpackButton:Hide()
+    CharacterBag0Slot:Hide()
+    CharacterBag1Slot:Hide()
+    CharacterBag2Slot:Hide()
+    CharacterBag3Slot:Hide()
 end
 
 -- Used to toggle bag with keybind.
 function ToggleBags()
-	--print("Shown:"..tostring(shown))
-	if not shown then
-		BagShow()
-		shown = true
-	else
-		BagHide()
-		shown = false
-	end
+    --print("Shown:"..tostring(shown))
+    if not shown then
+        BagShow()
+        shown = true
+    else
+        BagHide()
+        shown = false
+    end
 end
 
 -- Hides or shows bags on statup depending on saved variable.
 local function onEvent(self, event)
-	if shown then
-			BagShow()
-		else
-			BagHide()
-	end
+    if shown then
+            BagShow()
+        else
+            BagHide()
+    end
 end
 
 local addon = CreateFrame('Frame')
