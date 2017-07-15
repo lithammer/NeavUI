25a3439614308deb4a064f3e7428bc630e91e44c
Rainrider <rainrider.wow@gmail.com>
2017-07-16 01:21:21 +0200

    nChat: Use sane variables names

--- Interface/AddOns/nChat/copy.lua
+++ Interface/AddOns/nChat/copy.lua
@@ -6,75 +6,73 @@ local select = select
 local tostring = tostring
 local concat = table.concat
 
-    -- First, we create the copy frame
-
-local f = CreateFrame('Frame', nil, UIParent)
-f:SetHeight(220)
-f:SetBackdropColor(0, 0, 0, 1)
-f:SetFrameStrata('DIALOG')
-f:CreateBeautyBorder(12)
-f:SetBackdrop({
+local container = CreateFrame('Frame', nil, UIParent)
+container:SetHeight(220)
+container:SetBackdropColor(0, 0, 0, 1)
+container:SetFrameStrata('DIALOG')
+container:CreateBeautyBorder(12)
+container:SetBackdrop({
     bgFile = 'Interface\\DialogFrame\\UI-DialogBox-Background',
     edgeFile = '',
     tile = true, tileSize = 16, edgeSize = 16,
     insets = {left = 3, right = 3, top = 3, bottom = 3
 }})
-f:Hide()
+container:Hide()
 
-f.t = f:CreateFontString(nil, 'OVERLAY')
-f.t:SetFont('Fonts\\ARIALN.ttf', 18)
-f.t:SetPoint('TOPLEFT', f, 8, -8)
-f.t:SetTextColor(1, 1, 0)
-f.t:SetShadowOffset(1, -1)
-f.t:SetJustifyH('LEFT')
+local title = container:CreateFontString(nil, 'OVERLAY')
+title:SetFont('Fonts\\ARIALN.ttf', 18)
+title:SetPoint('TOPLEFT', container, 8, -8)
+title:SetTextColor(1, 1, 0)
+title:SetShadowOffset(1, -1)
+title:SetJustifyH('LEFT')
 
-f.b = CreateFrame('EditBox', nil, f)
-f.b:SetMultiLine(true)
-f.b:SetMaxLetters(20000)
-f.b:SetSize(450, 270)
-f.b:SetScript('OnEscapePressed', function()
-    f:Hide()
+local copyBox = CreateFrame('EditBox', nil, container)
+copyBox:SetMultiLine(true)
+copyBox:SetMaxLetters(20000)
+copyBox:SetSize(450, 270)
+copyBox:SetScript('OnEscapePressed', function()
+    container:Hide()
 end)
 
-f.s = CreateFrame('ScrollFrame', '$parentScrollBar', f, 'UIPanelScrollFrameTemplate')
-f.s:SetPoint('TOPLEFT', f, 'TOPLEFT', 8, -30)
-f.s:SetPoint('BOTTOMRIGHT', f, 'BOTTOMRIGHT', -30, 8)
-f.s:SetScrollChild(f.b)
+local scroll = CreateFrame('ScrollFrame', '$parentScrollBar', container, 'UIPanelScrollFrameTemplate')
+scroll:SetPoint('TOPLEFT', container, 'TOPLEFT', 8, -30)
+scroll:SetPoint('BOTTOMRIGHT', container, 'BOTTOMRIGHT', -30, 8)
+scroll:SetScrollChild(copyBox)
 
-f.c = CreateFrame('Button', nil, f, 'UIPanelCloseButton')
-f.c:SetPoint('TOPRIGHT', f, 'TOPRIGHT', 0, -1)
+local closeButton = CreateFrame('Button', nil, container, 'UIPanelCloseButton')
+closeButton:SetPoint('TOPRIGHT', container, 'TOPRIGHT', 0, -1)
 
-local function GetChatLines(self)
+local function GetChatLines(chat)
     local lines = {}
-    for message = 1, self:GetNumMessages() do
-        lines[message] = self:GetMessageInfo(message)
+    for message = 1, chat:GetNumMessages() do
+        lines[message] = chat:GetMessageInfo(message)
     end
 
     return lines
 end
 
-local function copyChat(self)
-    ToggleFrame(f)
+local function CopyChat(chat)
+    ToggleFrame(container)
 
-    if (f:IsShown()) then
-        local lines = GetChatLines(self)
+    if (container:IsShown()) then
+        local lines = GetChatLines(chat)
         if (cfg.showInputBoxAbove) then
-            local editBox = _G[self:GetName()..'EditBox']
-            f:SetPoint('BOTTOMLEFT', editBox, 'TOPLEFT', 3, 10)
-            f:SetPoint('BOTTOMRIGHT', editBox, 'TOPRIGHT', -3, 10)
+            local editBox = _G[chat:GetName()..'EditBox']
+            container:SetPoint('BOTTOMLEFT', editBox, 'TOPLEFT', 3, 10)
+            container:SetPoint('BOTTOMRIGHT', editBox, 'TOPRIGHT', -3, 10)
         else
-            local tabHeight = _G[self:GetName()..'Tab']:GetHeight()
-            f:SetPoint('BOTTOMLEFT', self, 'TOPLEFT', 0, tabHeight + 10)
-            f:SetPoint('BOTTOMRIGHT', self, 'TOPRIGHT', 0, tabHeight + 10)
+            local tabHeight = _G[chat:GetName()..'Tab']:GetHeight()
+            container:SetPoint('BOTTOMLEFT', chat, 'TOPLEFT', 0, tabHeight + 10)
+            container:SetPoint('BOTTOMRIGHT', chat, 'TOPRIGHT', 0, tabHeight + 10)
         end
 
-        f.t:SetText(self:GetName())
+        title:SetText(chat:GetName())
 
-        local f1, f2, f3 = self:GetFont()
-        f.b:SetFont(f1, f2, f3)
+        local f1, f2, f3 = chat:GetFont()
+        copyBox:SetFont(f1, f2, f3)
 
         local text = concat(lines, '\n')
-        f.b:SetText(text)
+        copyBox:SetText(text)
     end
 end
 
@@ -104,7 +102,7 @@ local function CreateCopyButton(self)
         self.Copy:GetNormalTexture():SetPoint('CENTER')
 
         if (self.Copy:IsMouseOver()) then
-            copyChat(self)
+            CopyChat(self)
         end
     end)
 end
