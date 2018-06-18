da16116714d0139850161e14468860d9c33a1e07
Peter Renström <renstrom.peter@gmail.com>
2018-06-18 09:43:23 +0200

    Got married 💍 and took my wife's last name

--- Interface/AddOns/nCore/modules/spellid.lua
+++ Interface/AddOns/nCore/modules/spellid.lua
@@ -35,7 +35,7 @@ GameTooltip:HookScript('OnTooltipSetSpell', function(self)
     local id = select(3, self:GetSpell())
     if (id) then
         -- Workaround for weird issue when this gets triggered twice on the Talents frame
-        -- https://github.com/renstrom/NeavUI/issues/76
+        -- https://github.com/lithammer/NeavUI/issues/76
         for i = 1, self:NumLines() do
             if _G['GameTooltipTextLeft'..i]:GetText() == 'SpellID: '..id then
                 return
