62ad0d103e514d7f2ee6a0f98d4ffb3553a00245
Thomas Shannon <thomasjohnshannon@gmail.com>
2018-08-09 17:20:25 -0600

    Add confirm to Alt+Click Invite
    Because why not. It was easy and this way you can avoid unintended invites.

--- Interface/AddOns/nChat/invite.lua
+++ Interface/AddOns/nChat/invite.lua
@@ -4,6 +4,20 @@ local _, nChat = ...
 local sub = string.sub
 local match = string.match
 
+StaticPopupDialogs["NCHAT_ALTCLICK"] = {
+    text = ERR_INVITE_PLAYER_S,
+    button1 = INVITE,
+    button2 = CANCEL,
+    OnAccept = function(self)
+        InviteUnit(self.data)
+    end,
+    timeout = 0,
+    whileDead = true,
+    hideOnEscape = true,
+    enterClicksFirstButton = true,
+    preferredIndex = 3,
+}
+
 local eventWatcher = CreateFrame("Frame")
 eventWatcher:RegisterEvent("ADDON_LOADED")
 
@@ -15,7 +29,7 @@ eventWatcher:SetScript("OnEvent", function(self, event, ...)
             local linkType = sub(link, 1, 6)
             if IsAltKeyDown() and linkType == "player" then
                 local name = match(link, "player:([^:]+)")
-                InviteUnit(name)
+                StaticPopup_Show("NCHAT_ALTCLICK", name, nil, name)
                 return nil
             end
 
