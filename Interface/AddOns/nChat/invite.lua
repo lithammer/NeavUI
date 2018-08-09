f893a3ff5411b281f2083135579f600b12a07bdc
Thomas Shannon <thomasjohnshannon@gmail.com>
2018-08-09 16:21:22 -0600

    nChat Alt+Click to Invite
    Hopefully this works without any issues now.

--- Interface/AddOns/nChat/invite.lua
+++ Interface/AddOns/nChat/invite.lua
@@ -1,18 +1,25 @@
--- Broken
 
--- local _, nChat = ...
+local _, nChat = ...
 
--- local sub = string.sub
--- local match = string.match
+local sub = string.sub
+local match = string.match
 
--- local origSetItemRef = SetItemRef
--- function SetItemRef(link, text, button)
-    -- local linkType = sub(link, 1, 6)
-    -- if IsAltKeyDown() and linkType == "player" then
-        -- local name = match(link, "player:([^:]+)")
-        -- InviteUnit(name)
-        -- return nil
-    -- end
+local eventWatcher = CreateFrame("Frame")
+eventWatcher:RegisterEvent("ADDON_LOADED")
 
-    -- return origSetItemRef(link, text, button)
--- end
+eventWatcher:SetScript("OnEvent", function(self, event, ...)
+    local name = ...
+    if name == "Blizzard_CombatLog" then
+        local origSetItemRef = SetItemRef
+        function SetItemRef(link, text, button, chatFrame)
+            local linkType = sub(link, 1, 6)
+            if IsAltKeyDown() and linkType == "player" then
+                local name = match(link, "player:([^:]+)")
+                InviteUnit(name)
+                return nil
+            end
+
+            return origSetItemRef(link, text, button, chatFrame)
+        end
+    end
+end)
